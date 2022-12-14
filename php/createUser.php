9e70ee88cee76a2dcb302fec6d4c5b473eb3882d
ariclavel <a01706629@itesm.mx>
2022-12-14 10:15:29 -0400

    hey

--- php/createUser.php
+++ php/createUser.php
@@ -7,9 +7,9 @@ $person_id = $_GET['email'];
 $username = $_GET['email']; 
 $email = $_GET['email'];
 $password = $_GET['pss1'];
-$first_name = "Ariana",
-$last_name = "Ayaviri",
-$phoneNumber = "11111"
+$first_name = "Ariana";
+$last_name = "Ayaviri";
+$phoneNumber = "11111";
 
 
 $result = Connection($person_id, $username, $email, $password, $first_name, $last_name, $phoneNumber);
