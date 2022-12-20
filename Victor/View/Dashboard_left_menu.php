3dcfb36529f6a9f2d5d2192ba126dffe39a0b5f7
victorhabila <48987601+victorhabila@users.noreply.github.com>
2022-12-20 16:24:17 +0100

    update on the edit pet

--- Victor/View/Dashboard_left_menu.php
+++ Victor/View/Dashboard_left_menu.php
@@ -3,8 +3,8 @@
         <span>Main Menu</span>
         <a href="Dashboard.php" class="active">Dashboard</a>
         <a href="userprofile2.php">Profile</a>
-        <a href="editPets.php">Pets</a>
-        <a href="../../html/devices.html">Devices</a>
+        <a href="Pets2.php">Pets</a>
+        <a href="adding_and_remove_devices.php">Devices</a>
         <a href="history.php">History</a>
         <a href="history.php">Scheduler</a>
         <a href="history.php">Reports</a>
