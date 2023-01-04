6b6c45ba6e5fb267698d9300c042cddbdc45e1b9
MarkMalish <marks.malyshkins@gmail.com>
2023-01-04 19:25:53 +0100

    Create doors2.php file
    updating html/css in doors file

---
+++ App/View/doors2.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Dashboard | Happy Family</title>
+  <link rel="stylesheet" href="../css/user_profile_victor.css">
+  
+  <!-- Font Awesome Cdn Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+  <link rel="stylesheet" href="../css/dashboard.css">
+  
+  <style type="text/css">a
+   .text-danger{
+    color:red;
+   }
+   .alert-info{
+    color:green;
+   }
+   .main-body{
+    margin-right: 230px;
+   }
+   .side_navbar{
+    background-color:#fff;
+   }
+   #imm{
+    padding: 15px;
+   }
+   .btnn, #btnn{
+    background: #f85508;
+    border:none;
+    color:white;
+   }
+   .user_email
+   {
+    margin-left:30px;
+   }
+
+   .both{
+    display:flex;
+   }
+ 
+  </style>
+</head>
+<body>
+<?php
+include("Dashboard_header.php");
+?>
+
+ 
+  <div class="container">
+  <?php
+include("Dashboard_left_menu.php");
+?>
+   
+
+<div class="main-body">
+   
+<div class="content">
+
+</div>
+</div>
+</div>
+</body>
+</html>
