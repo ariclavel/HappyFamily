349b22f02ff3915a50ebdc658eccaf754aa32b52
Kentmatisse <ktadjouddine@gmail.com>
2023-01-04 15:49:52 +0100

    876

--- App/View/Contact_us2.php
+++ App/View/Contact_us2.php
@@ -57,92 +57,106 @@ include("Dashboard_left_menu.php");
 ?>
    
 
-<div class="main-body">
-   
-<div class="content">
+   <div class="content">
    
    
-<div class="profile">
-  <div class="middle_part">
-        <div class="settings">
-            <h4 class="text-right">Edit pet</h4>
-        </div>
-
-        <form name="form1" id="form1" action="/action_page.php">
-        <div class="both">
-             <div class="both_1">
-                    <div class="col-md-12">
-                      <select class="form-control" name="subject" id="subject">
-                        <option value="" selected="selected">Select category</option>
-                        <option value="" >Dogs</option>
-                        <option value="" >Cats</option>
     
-                      </select>
-                    </div>  
-                  
-                    <div class="col-md-12"><input type="text" class="form-control" placeholder="Pet name" value=""></div>
-                    
-                          <div class="col-md-12">
-                            <label class="labels">Description</label><br/>
-                            <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
-                           </div>
-                           <div class="mt-5 text-center"><button class="btnn" type="button">Update</button></div>
-                    </div>
-                    
-                    <div class="both_2">
-             
-                            
+    <div class="profile">
+       
         
-                            <div class="profile_pxx">
-                                 <img class="image2" width="150px" height="150px" src="../img/pet2.png"><br/><br/><span class="pet_name">Name: Nanny-p</span>
-                                <div ><button type="button" class="update_pet">change photo</button></div>
-                                
-                            </div>
-                       
-               </div>
-               </div>
-                </div>
 
-
-                
-        </form>
-         
-
-            </div>
-           
-           
-            <div class="modal">
-                <div class="modal-content">
-                    <span class="close-button">×</span>
-                    <h1>Update pet photo</h1>
-                    <form action="/action_page.php">
-                        <input type="file" id="myFile" name="filename">
-                        <button type="submit" name ="update">upload</button>
-                   
+    
+    <div class="middle_part">
+        <section class="footer_get_touch_outer">
+            <div class="container">
+              <div class="footer_get_touch_inner grid-70-30">
+                <div class="colmun-70 get_form">
+                  <div class="get_form_inner">
+                    <div class="get_form_inner_text">
+                      <h3>Get In Touch</h3>
+                    </div>
+                    <form action="#">
+                      <div class="grid-50-50">
+                        <input type="text" placeholder="First Name">
+                        <input type="text" placeholder="Last Name">
+                        <input type="email" placeholder="Email">
+                        <input type="tel" placeholder="Phone/Skype">
+                      </div>
+                      <div class="grid-full">
+                        <textarea placeholder="About Your Project" cols="30" rows="10"></textarea>
+                        <input type="submit" value="Submit">
+                      </div>
                     </form>
+                  </div>
                 </div>
+                <div class="colmun-30 get_say_form">
+                  <h5>Say Hi!</h5>
+                  <ul class="get_say_info_sec">
+                    <li>
+                      <i class="fa fa-envelope"></i>
+                      <a href="mailto:">info@Happyhome.com</a>
+                    </li>
+                    <li>
+                      <i class="fa fa-whatsapp"></i>
+                      <a href="tel:">+339602381997</a>
+                    </li>
+                    <li>
+                      <i class="fa fa-skype"></i>
+                      <a href="#">Stackfindover</a> 
+                    </li>
+                  </ul>  
+                  <ul class="get_say_social-icn">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                  </ul>          
+                </div>        
+              </div>
             </div>
+          </section>
             
     </div>
 
 
-       </div>
-       </div>
-
-   </div>
 
 
-</div>
-  
- 
 
-   
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+        </div>
+        </div>
 
     </div>
 
 
-  </div>
+
+
+</div>
+
+<div class="footer">&copy; Smart Home group - Smarthome.com</div>
+
+</div>
   
 
 <script>
