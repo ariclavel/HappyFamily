c08fa18bb9a1995616c167c7622a4f0177cc05c9
ariclavel <a01706629@itesm.mx>
2023-01-18 10:11:55 -0400

    yes

--- App/View/Dashboard_left_menu.php
+++ App/View/Dashboard_left_menu.php
@@ -3,14 +3,10 @@
         <span>Main Menu</span>
         <a href="Dashboard.php" class="active">Dashboard</a>
         <a href="userprofile2.php">Profile</a>
-
         <a href="chat.php">Messages</a>
         <a href="history.php">History</a>
-
         <a href="petDashboard_victor.php">Pets</a>
         <a href="Add_device_victor.php">Devices</a>
-
-
         <a href="scheduler.php">Scheduler</a>
         <a href="Alarm.php">Alarm</a>
         <a href="history.php">Reports</a>
