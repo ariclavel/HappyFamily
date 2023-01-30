d1ae973ac60d818622d04902163392d40ce04e30
victorhabila <48987601+victorhabila@users.noreply.github.com>
2023-01-31 00:05:05 +0100

    add pet update

--- App/View/Dashboard_left_menu.php
+++ App/View/Dashboard_left_menu.php
@@ -3,7 +3,8 @@
         <span>Main Menu</span>
         <a href="DashboardUser.php" class="active">Dashboard</a>
         <a href="userprofile2.php">Profile</a>
-        <a href="petDashboard_victor.php">Pets</a>
+        <a href="petDashboard_victor.php">Pet Dashboard</a>
+        <a href="Add_pet.php">Add a pet</a>
         <a href="viewDevices_victor.php">Devices</a>
         <a href="scheduler.php">Scheduler</a>
         <a href="Alarm.php">Alarm</a>
